e8183027f88cd3fdb1615e59b4c3ea4715cf0a24
Saket Mishra <saketmihsra5454@gmail.com>
2024-09-18 23:11:42 +0530

    Added Bubble sort

--- bubbleSort.java
+++ bubbleSort.java
@@ -22,7 +22,7 @@ class bubbleSort{
         // System.out.println(sort(arr, n));
         sort(arr, arr.length);
         for(int i = 0 ; i< arr.length; i++){
-            System.out.print(arr[i]);
+            System.out.print(arr[i]+" ");
         
         }
     }
